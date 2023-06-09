2fb9710e827dc410041d7973b3825ea643afddb9
pc21 <pc21@pc21>
2023-06-09 10:56:48 +0900

    2023 06 09

---
+++ src/main/java/com/nextit/www/service/NextITApiService.java
@@ -0,0 +1,26 @@
+package com.nextit.www.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nextit.www.NextITApiController;
+
+import kr.or.nextit.code.service.ICommCodeService;
+
+@Service
+public class NextITApiService {
+
+//	@Autowired
+//	private NextITApiController apiController;
+
+	
+	public void apiJoin() {
+		System.out.println("NextITApiService apiJoin");
+
+		
+//		apiController.apiJoin();
+	}
+
+}
